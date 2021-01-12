373fea7424642febcebcc33ea46669fbe1b29476
Septian139 <58352120+Septian139@users.noreply.github.com>
2021-01-12 21:53:28 +0700

    Fix Expression Builder

--- seal/Helper/ExpressionBuilder.cs
+++ seal/Helper/ExpressionBuilder.cs
@@ -20,138 +20,62 @@ namespace seal.Helper
             return new ExpressionBuilder(str);
         }
 
-        public ExpressionBuilder And(ExpressionBuilder eb)
-        {
-            buffer += " AND " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder Or (ExpressionBuilder eb)
-        {
-            buffer += " OR " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder Like(ExpressionBuilder eb)
-        {
-            buffer += " LIKE " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder Between(ExpressionBuilder eb1, ExpressionBuilder eb2)
-        {
-            buffer += " BETWEEN " + eb1.buffer + " AND " + eb2;
-            return this;
-        }
-
-        public ExpressionBuilder In(IList<ExpressionBuilder> str)
-        {
-            buffer += " IN (";
-            bool first = true;
-            foreach (ExpressionBuilder s in str)
-            {
-                if (!first)
-                {
-                    buffer += ",";
-                }
-                buffer += s.buffer;
-            }
-            buffer += ")";
-            return this;
-        }
-
-        public ExpressionBuilder In(params ExpressionBuilder[] str)
-        {
-            buffer += " IN (";
-            bool first = true;
-            foreach (ExpressionBuilder s in str)
-            {
-                if (!first)
-                {
-                    buffer += ",";
-                }
-                buffer += s.buffer;
-            }
-            buffer += ")";
-            return this;
-        }
-
-        public ExpressionBuilder In(params string[] str)
-        {
-            buffer += " IN (";
-            bool first = true;
-            foreach (string s in str)
-            {
-                if (!first)
-                {
-                    buffer += ",";
-                }
-                buffer += s;
-            }
-            buffer += ")";
-            return this;
-        }
-
-        public ExpressionBuilder In(IList<string> str)
-        {
-            buffer += " IN (";
-            bool first = true;
-            foreach (string s in str)
-            {
-                if (!first)
-                {
-                    buffer += ",";
-                }
-                buffer += s;
-            }
-            buffer += ")";
-            return this;
-        }
-
-        public ExpressionBuilder Equal(ExpressionBuilder eb)
-        {
-            buffer += " = " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder NotEqual(ExpressionBuilder eb)
-        {
-            buffer += " <> " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder GreaterThan(ExpressionBuilder eb)
-        {
-            buffer += " > " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder SmallerThan(ExpressionBuilder eb)
-        {
-            buffer += " < " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder GreaterThanOrEqual(ExpressionBuilder eb)
-        {
-            buffer += " >= " + eb.buffer;
-            return this;
-        }
-
-        public ExpressionBuilder SmallerThanOrEqual(ExpressionBuilder eb)
-        {
-            buffer += " <= " + eb.buffer;
-            return this;
-        }
-
         public static ExpressionBuilder Group(ExpressionBuilder eb)
         {
             string buffer = "(";
-            buffer += (string)eb;
+            buffer += eb.buffer;
             buffer += ")";
             return new ExpressionBuilder(buffer);
         }
         public ExpressionBuilder(string expression) { buffer = expression; }
-        public ExpressionBuilder() {}
+        public ExpressionBuilder(ExpressionBuilder left, LogicalOperator opr, params ExpressionBuilder[] right)
+        {
+            buffer = left.buffer + " " + opr.ToString();
+            if(opr == LogicalOperator.BETWEEN)
+            {
+                buffer += " " + right[0].buffer + " AND " + right[1].buffer;
+                return;
+            }
+
+            if(opr == LogicalOperator.In)
+            {
+                buffer += " {";
+                bool first = true;
+                foreach(ExpressionBuilder eb in right)
+                {
+                    if (!first) buffer += ", ";
+                    buffer += eb.buffer;
+                }
+                buffer += "}";
+                return;
+            }
+
+            buffer += " " + right[0];
+        }
+
+        public ExpressionBuilder(LinkOperator link, ExpressionBuilder left, LogicalOperator opr, params ExpressionBuilder[] right)
+        {
+            buffer = link.ToString() + " " + left.buffer + " " + opr.ToString();
+            if (opr == LogicalOperator.BETWEEN)
+            {
+                buffer += " " + right[0].buffer + " AND " + right[1].buffer;
+                return;
+            }
+
+            if (opr == LogicalOperator.In)
+            {
+                buffer += " {";
+                bool first = true;
+                foreach (ExpressionBuilder eb in right)
+                {
+                    if (!first) buffer += ", ";
+                    buffer += eb.buffer;
+                }
+                buffer += "}";
+                return;
+            }
+
+            buffer += " " + right[0];
+        }
     }
 }
